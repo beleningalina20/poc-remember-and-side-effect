ba84933b8bb6f090d568728478b7e6923d8cd135
Belen Ingalina <ingalinaa.mb@gmail.com>
2025-12-29 16:29:08 -0300

    Counter is not updating using a regular variable

--- app/src/main/java/com/beleningalina/jetpackcomposepoc/MainActivity.kt
+++ app/src/main/java/com/beleningalina/jetpackcomposepoc/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.beleningalina.jetpackcomposepoc.ui.theme.JetpackComposePOCTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +23,41 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposePOCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Counter(modifier = Modifier.padding(innerPadding))
+
                 }
             }
+        }
+    }
+}
+
+
+/**
+ * This counter does NOT work as expected.
+ *
+ * - `counter` is a regular Int, not a State
+ * - Updating a normal variable does not trigger recomposition
+ * - Jetpack Compose only observes State changes
+ * - As a result, the UI never updates
+ *
+ * Not every variable is State.
+ * Use State (read-only) or MutableState (mutable) to update the UI.
+ */
+@Composable
+fun Counter(modifier: Modifier = Modifier) {
+    var counter: Int = 0
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("$counter")
+        Button(
+            onClick = {
+                counter++
+            }
+        ) {
+            Text("+")
         }
     }
 }
